982aeefc5c558dfa615b798019e14c3eb2c03d39
TiagoPavloski <t_pavloski@hotmail.com>
2017-09-12 15:18:15 -0300

    Tiago - Retirei o negócio chato ali

---
+++ sources/Norvana/src/br/edu/norvana/rest/ProdutoRest.java
@@ -0,0 +1,42 @@
+package br.edu.norvana.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import br.edu.norvana.Dao.LocalDao;
+import br.edu.norvana.Facade.Facade;
+import br.edu.norvana.business.BusinessException;
+import br.edu.norvana.entity.Produto;
+
+@Path("/produtorest")
+public class ProdutoRest {
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void cadastrarProduto(Produto produto){
+		
+		Facade facade = new Facade();
+		
+		try {
+			facade.salvar(produto);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+			
+	}
+	
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArrayList<Produto> listarProduto(){
+		return (ArrayList<Produto>) new Facade().listarProduto();
+		
+				
+	}
+
+}
